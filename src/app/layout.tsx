5ca0242bf04130fce05f8bfe4cfaabe1276a26f3
Javat <javiervillagomez96@gmail.com>
2025-06-14 19:38:05 -0600

    edit texts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { Analytics } from "@vercel/analytics/next"
 import "@/styles/globals.css";
 import { Montserrat } from 'next/font/google'
 import NavBar from "@/components/ux/nav-bar";
