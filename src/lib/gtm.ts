1526c8bb25f55d379570f4b7482eb925f8746d05
Javat <javiervillagomez96@gmail.com>
2025-06-27 22:33:12 -0600

    privacy policy

--- src/lib/gtm.ts
+++ src/lib/gtm.ts
@@ -1,15 +1,29 @@
 // utils/gtm.ts
 
+
+
 interface GTMEvent {
+
   event: string;
-  [key: string]: any;
+
+  [key: string]: any; // Permite propiedades adicionales
+
 }
 
+
+
 export const sendGTMEvent = (eventData: GTMEvent) => {
-  if (typeof window !== 'undefined' && typeof (window as { dataLayer?: any[] }).dataLayer !== 'undefined') {
-    (window as { dataLayer: any[] }).dataLayer.push(eventData);
-    console.log('GTM Event pushed:', eventData);
+
+  if (typeof window !== 'undefined' && (window as any).dataLayer) {
+
+    (window as any).dataLayer.push(eventData);
+
+    console.log('GTM Event pushed:', eventData); // Opcional: para depuración
+
   } else {
+
     console.warn('dataLayer not found or not in browser environment. GTM event not sent:', eventData);
+
   }
+
 };
